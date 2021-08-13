99707127b926d7000d3d546e60179ca513af0126
GitHub Action <action@github.com>
2021-08-13 11:28:52 +0000

    Template cleanup

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-const val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+const val PUBLISHING_GROUP = "com.github.readbeard.openbrewery"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "com.github.readbeard.openbrewery.app"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("openbrewery")
 
 include(
     "app",
