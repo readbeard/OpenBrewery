9a32f495cbd7f0c061786d6fe99110e916798a11
Matteo Principe <matteo.principe@keyless.io>
2021-08-16 22:27:08 +0200

    Missing annotation in MainActivity
    Signed-off-by: Matteo Principe <matteo.principe@keyless.io>

--- app/src/main/java/com/readbeard/openbrewery/app/MainActivity.kt
+++ app/src/main/java/com/readbeard/openbrewery/app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.readbeard.openbrewery.app
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 import com.readbeard.openbrewery.app.beerlist.presentation.BreweryScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -11,6 +12,7 @@ import kotlinx.coroutines.FlowPreview
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @ExperimentalFoundationApi
     @FlowPreview
     @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
