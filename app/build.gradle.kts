ea272af8a6055e659b219c6ff18d3f673d4b39fc
Matteo Principe <matteo.principe@keyless.io>
2021-08-16 22:17:10 +0200

    Try to avoid crash on gradle lint fail
    Signed-off-by: Matteo Principe <matteo.principe@keyless.io>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     }
 
     lintOptions {
-        isWarningsAsErrors = true
+        isWarningsAsErrors = false
         isAbortOnError = true
     }
 
--- library-android/build.gradle.kts
+++ library-android/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     }
 
     lintOptions {
-        isWarningsAsErrors = true
+        isWarningsAsErrors = false
         isAbortOnError = true
     }
 }
